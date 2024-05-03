f4e6febfc2d0321708e05efa85203f968aacddf2
Rasa <rassa.kundrotaite@gmail.com>
2024-05-03 15:16:13 +0300

    added model training

---
+++ RNN_model/model.py
@@ -0,0 +1,18 @@
+from tensorflow.keras import layers, Input, Sequential
+
+def RNN_bilstm(datanum):
+
+  model = Sequential()
+  model.add(Input(shape=(datanum,1)))
+  model.add(layers.Bidirectional(layers.LSTM(units=1, return_sequences=True)))
+
+  model.add(layers.Dropout(0.3))
+
+  model.add(layers.Bidirectional(layers.LSTM(units=1, return_sequences=False)))
+  model.add(layers.Dropout(0.2))
+
+  model.add(layers.Dense(units=datanum, activation="relu"))
+  model.add(layers.Dense(datanum))
+  model.summary()
+
+  return model
